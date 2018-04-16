9c2071e81a07b2cf2690d67459d397988271a94a
Terry <baosl_886@126.com>
2018-04-16 16:12:36 +0800

    no message

---
+++ Chained/SLBaseObject.h
@@ -0,0 +1,13 @@
+//
+//  SLBaseObject.h
+//  Chained
+//
+//  Created by Terry on 2018/4/16.
+//  Copyright © 2018年 Terry. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SLBaseObject : NSObject
+
+@end
